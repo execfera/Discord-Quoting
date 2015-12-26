ce3b2c7484f1d4f67da206636f0848b0c795162d
Ghost <ghost@ghosted.me>
2015-12-26 14:03:40 +0000

    Update Quoting.js

--- Quoting.js
+++ Quoting.js
@@ -1,133 +1,166 @@
-inject(); //0.1.5
-//run();  //0.1.3
-//update(); //unknown
+//META{"name":"DCMQuotingPlugin"}*// 
 
-function getMessage(element) {
-    var msg = "";
-    
-    while (!(element.classList.contains("message-group")))
-        element = element.parentElement;
-    
-    var body = element.getElementsByClassName("first")[0]
-        .getElementsByClassName("body")[0];
-        
-    var time = body.getElementsByTagName("h2")[0]
-        .getElementsByTagName("span")[0].innerText;
+/*
+  Crossplatform script
+*/
 
-    var username = body.getElementsByTagName("h2")[0]
-        .getElementsByClassName("user-name")[0].innerText;
-    
-    var comments = element.getElementsByClassName("comment")[0]
-        .getElementsByClassName("message");
-    
-    for (var i = 0, im = comments.length; im > i; i++) {
-        var text = comments[i].getElementsByClassName("markup")[0]
-            .innerText
-            .replace("(edited)", "")
-            .replace("\n\r", "")
-            .replace("_", "");
-            
-        if (!(text == ""))
-            msg = msg + "[" + time + "] " + username + ": _" + text + "_\n" ;
-    }
-    return msg;
-}
+//This class is for hacky QUOTE INJECTION only
+function DCMQuotingPlugin(){
+    var ghostModId = 2;
 
-function checkVal(a) {
-    if (typeof a == "undefined")
-        return 300;
-    if (typeof a.getElementsByTagName("h2")[0] == "undefined")
-        return 300;
-    return a.getElementsByTagName("h2")[0].childNodes.length;
-}
-
-function clicked(messageElement) {
-    var textArea = document.getElementsByTagName("textarea")[0];
-    const message = getMessage(messageElement);
-    const oldMsg = textArea.value;
-    
-    textArea.value = (oldMsg == "" ? oldMsg : oldMsg + "\n") + message + "\n";    //append if text is already in the text box
-
-    resize(textArea);
-}
-
-function resize(textArea){
-    textArea.onkeydown = function() {
-        var key = event.keyCode || event.charCode;
-        if(key == 8 || key == 46){
-            resize(this);
-        }
+    this.load = function(){
+        inject();
     };
-    textArea.style.height = textArea.scrollHeight > textArea.clientHeight ? (textArea.scrollHeight) + "px" : "60px";
-}
 
-function inject() {
-    document.addEventListener("DOMNodeInsertedIntoDocument", function() {
-        update();
-    }, false);
-    document.addEventListener("DOMNodeInserted", function() {
-        update();
-    }, false);
-}
+    this.start = function(){
+        inject(); 
+    };
 
+    this.unload = function(){
+        removeAllEvents(document, "DOMNodeInsertedIntoDocument");
+        removeAllEvents(document, "DOMNodeInserted");
+    };
 
-//Run whilst you can
-//__________                                 .__     .__ .__             __                                                            ._.
-//\______   \ __ __   ____          __  _  __|  |__  |__||  |    _______/  |_         ___.__.  ____   __ __       ____  _____     ____ | |
-// |       _/|  |  \ /    \         \ \/ \/ /|  |  \ |  ||  |   /  ___/\   __\       <   |  | /  _ \ |  |  \    _/ ___\ \__  \   /    \| |
-// |    |   \|  |  /|   |  \         \     / |   Y  \|  ||  |__ \___ \  |  |          \___  |(  <_> )|  |  /    \  \___  / __ \_|   |  \\|
-// |____|_  /|____/ |___|  /          \/\_/  |___|  /|__||____//____  > |__|          / ____| \____/ |____/      \___  >(____  /|___|  /__
-//        \/             \/                       \/                \/                \/                             \/      \/      \/ \/
+    this.stop = function(){
+        removeAllEvents(document, "DOMNodeInsertedIntoDocument");
+        removeAllEvents(document, "DOMNodeInserted");
+    };
 
+    var checkVal = function(a){
+        if (typeof a == "undefined")
+            return 300;
+        if (typeof a.getElementsByTagName("h2")[0] == "undefined")
+            return 300;
+        return a.getElementsByTagName("h2")[0].childNodes.length;
+    };
 
-//The code powering this is really hackky and was never intended to be released
-//    You may not want to witness anything past this lin
+    var inject = function(){
+        DCMQuoting.enabled = true;
+        document.addEventListener("DOMNodeInsertedIntoDocument", function() {
+            update();
+        }, false);
+        document.addEventListener("DOMNodeInserted", function() {
+            update();
+        }, false);
+    };
 
+    var createSpan = function(){
+        var span = document.createElement("span");
+        span.setAttribute("style", "display:inline-block");
+        span.innerText = "Quote";
+        span.setAttribute("onclick", "DCMQuoting.clicked(this);");
+        return span;
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function update(){
-    if ((typeof(document.getElementsByClassName("messages")[0]) !== 'undefined') 
-            && (document.getElementsByClassName("messages")[0] !== null)) {
-        var elements = document.getElementsByClassName("messages")[0]
-            .getElementsByTagName("div");
-        for (var i = 0, im = elements.length; im > i; i++) {
-            var element = elements[i]
-                .getElementsByTagName("span");
-            for (var ia = 0, ima = element.length; ima > ia; ia++) {
-                var content = element[ia].parentElement.parentElement;
-                if ((content.className == "body") && (checkVal(content) == 2))
-                    content.getElementsByTagName("h2")[0].appendChild(createSpan())
+    //Still no good way to get all messages with BetterDiscord (afaik meaning I'm probably wrong)... copy OP whilst using a mod id (mod id = expected amount of ghosts scripted installed + 2)
+    var update = function(){
+        if ((typeof(document.getElementsByClassName("messages")[0]) !== 'undefined') 
+                && (document.getElementsByClassName("messages")[0] !== null)
+                && (DCMQuoting.enabled)) {
+            var elements = document
+                .getElementsByClassName("messages")[0]
+                .getElementsByTagName("div");
+            for (a of elements) {
+                var elementsC = a.getElementsByTagName("span");
+                for (element of elementsC) {
+                    var content = element.parentElement.parentElement;
+                    if ((content.className == "body") && (checkVal(content) == ghostModId))
+                        content.getElementsByTagName("h2")[0].appendChild(createSpan())
+                }
             }
         }
-    }
-}
+    };
+};
+ 
 
-function createSpan(){
-    var span = document.createElement("span");
-    span.setAttribute("style", "display:inline-block");
-    span.innerText = "Quote";
-    span.setAttribute("onclick", "clicked(this);");
-    return span;
+DCMQuotingPlugin.prototype.getName = function() { 
+    return "Quoting"; 
+}; 
+
+DCMQuotingPlugin.prototype.getDescription = function() { 
+    return "Quoting from Discord Client Modding ported by NotGGhost"; 
+}; 
+ 
+DCMQuotingPlugin.prototype.getVersion = function() { 
+    return "0.1.7"; 
+}; 
+
+DCMQuotingPlugin.prototype.getAuthor = function() { 
+    return "Ghost"; 
+}; 
+
+DCMQuotingPlugin.prototype.getSettingsPanel = function() { 
+    return '<center><img src="https://s14.postimg.org/6w6z0pdpd/NJa3g_V_1.png"></img><br><b style="font-size: 40px;"> Nothing to see here yet... </b></center>'; 
+}; 
+
+var CDCMQuoting = function(){
+    this.enabled = true;
+
+    this.getMessage = function(element) {
+        var msg = "";
+        
+        while (!(element.classList.contains("message-group")))
+            element = element.parentElement;
+        
+        var body = element.getElementsByClassName("first")[0]
+            .getElementsByClassName("body")[0];
+            
+        var time = body.getElementsByTagName("h2")[0]
+            .getElementsByTagName("span")[0]
+            .innerText
+            .replace("Today at ", "");
+
+        var username = body.getElementsByTagName("h2")[0]
+            .getElementsByClassName("user-name")[0].innerText;
+        
+        var comments = element.getElementsByClassName("comment")[0]
+            .getElementsByClassName("message");
+        
+        for (comment of comments) {
+            var text = comment.getElementsByClassName("markup")[0]
+                .innerText
+                .replace("(edited)", "")
+                .replace("\n\r", "")
+                .replace("_", "");
+                
+            if (!(text == ""))
+                msg = msg + "[" + time + "] " + username + ": _" + text + "_\n" ;
+        }
+        return msg;
+    };
+
+    this.resize = function(textArea){
+        textArea.onkeydown = function() {
+            var key = event.keyCode || event.charCode;
+            if(key == 8 || key == 46)
+               DCMQuoting.resize(this);
+        };
+        textArea.style.height = textArea.scrollHeight > textArea.clientHeight ? (textArea.scrollHeight) + "px" : "18px";
+    }
+
+    this.clicked = function(messageElement){
+        var textArea = document.getElementsByTagName("textarea")[0];
+        const message = this.getMessage(messageElement);
+        const oldMsg = textArea.value;
+        var quote = (oldMsg == "" ? oldMsg : oldMsg + "\n") + message + "\n";    //append if text is already in the text box
+        
+        $(textArea).focus().val("").val(quote);
+        this.resize(textArea);
+        textArea.scrollTop = textArea.scrollHeight;
+    };
+};
+
+var DCMQuoting = new CDCMQuoting();
+
+
+//TODO: http://stackoverflow.com/questions/4386300/javascript-dom-how-to-remove-all-events-of-a-dom-object 
+function removeAllEvents(node, event) {
+    DCMQuoting.enabled = false;
+};
+
+if (!((typeof(betterDiscordIPC) !== 'undefined') && (betterDiscordIPC !== null))) {
+    var str = "Warning: This Discord Quoting script is designed to work in BetterDiscord only!\nHOWEVER it is still trying to load\n\n(Discord Client Modding is deprecated)";
+    console.log(str);
+    alert(str);
+    new DCMQuotingPlugin().start();
 }
